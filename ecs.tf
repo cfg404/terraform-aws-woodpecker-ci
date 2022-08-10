d6e107a4082d732d017542387c36c50f72428c9c
Ovidiu Calbajos <ovidiu.calbajos@gmail.com>
2022-08-10 14:53:29 +0300

    Try for each setting check if null

--- ecs.tf
+++ ecs.tf
@@ -31,7 +31,7 @@ resource "aws_ecs_cluster" "this" {
   }
 
   dynamic "setting" {
-    for_each = [var.cluster_settings]
+    for_each = try([var.cluster_settings], [])
 
     content {
       name  = setting.value.name
